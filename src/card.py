07bf2a6225192cd39c49783d0aefdd6ca654334a
Luciano Pereira Sampaio <luciano.sampaio2004@gmail.com>
2023-04-25 08:19:34 -0300

    __eq__ function made

--- src/card.py
+++ src/card.py
@@ -5,3 +5,9 @@ class Card():
     def __init__(self, suit: Suit, number: Number):
         self.suit = suit
         self.number = number
+    
+    def __eq__(self, other):
+        if not isinstance(other, self):
+            return False
+        
+        return self.suit == other.suit and self.number == other.number
