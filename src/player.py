3c73dc785b14d6ddf0b57bcac81cc01b743485d7
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-04-26 07:56:46 -0300

    Method to get score implemented

--- src/player.py
+++ src/player.py
@@ -10,4 +10,14 @@ class Player:
         
     def take_card(self, card:Card):
         self.cards.append(card)
-        
+    
+    def score(self):
+        return sum([int(i) for i in self.cards])
+    
+
+if __name__=="__main__":
+    player = Player('Jorge')
+    player.take_card(Card(Suit.SPADES, Number.ACE))
+    print(player.score())
+    player.take_card(Card(Suit.HEARTS, Number.QUEEN))
+    print(player.score())  
